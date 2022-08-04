b094753663faf9368b6e5e41890d042d557b3144
mik985 <myachmenkov@gmail.com>
2022-08-05 00:58:21 +0300

    pre-commit hook configuration for flake8

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import os
 
+
 def func():
 	print("func() was called")
 
+
 if __name__ == "__main__":
-	print("script was called as main")
+  	print("script was called as main second time \n")
+	print(os.path)
+	
